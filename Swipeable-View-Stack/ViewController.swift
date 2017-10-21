362c6307b544fb074329366030b3116dd6285c2c
Phill Farrugia <me@phillfarrugia.com>
2017-10-21 10:11:00 -0700

    Addes Empty Xcode Project

---
+++ Swipeable-View-Stack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swipeable-View-Stack
+//
+//  Created by Phill Farrugia on 10/21/17.
+//  Copyright © 2017 Phill Farrugia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
